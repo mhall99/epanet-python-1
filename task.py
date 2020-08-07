3afc89c6f95e454634fbe3c1c1908767daf71b05
Mac <66694993+mhall99@users.noreply.github.com>
2020-08-07 13:59:27 -0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,39 @@
+# -*- coding -*-
+import os
+import epanet_module.epamodule as em
+
+def HydraAna( inputFile, MyNode ):
+    #Open the EPANET toolkit & hydraulic analysis, load 3-node system
+    em.ENopen(inputFile, "task.rpt")
+    em.ENopenH()
+
+    #sets duration of analysis to 36000 seconds (10 hours)
+    em.ENsettimeparam(em.EN_DURATION, 36000)
+    nodeCount = em.ENgetcount(em.EN_NODECOUNT)
+    linkCount = em.ENgetcount(em.EN_LINKCOUNT)
+
+    #initializes tank volumes, link status, settings, duration, etc
+    em.ENinitH()
+
+    #runs single hydraulic analysis
+    em.ENrunH()
+
+    #pressureList = []
+    #demandList = []
+    #headList = []
+
+    #node stuff
+    #while nodeCount != 0:
+    nodeindex = em.ENgetnodeindex(MyNode);
+    pressure = em.ENgetnodevalue(nodeindex, em.EN_PRESSURE)
+    demand = em.ENgetnodevalue(nodeindex, em.EN_DEMAND)
+    head = em.ENgetnodevalue(nodeindex, em.EN_HEAD)
+
+    #em.ENsolveQ()
+    #link stuff 
+    #linkindex = em.ENgetlinkindex(linkid);
+
+    print('Node index is %d, node pressure is %d, node demand is %d, node head is %d.' % (nodeindex, pressure, demand, head))
+
+HydraAna("Threenode-cl-2.inp","2")
+
